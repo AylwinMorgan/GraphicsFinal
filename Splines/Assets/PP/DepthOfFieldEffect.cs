f439feab843cd89cf873c98ba91e24571515b07f
Sierra Blume <sierra.blume@mymail.champlain.edu>
2025-04-22 09:50:57 -0400

    Working on depth of field

--- Splines/Assets/PP/DepthOfFieldEffect.cs
+++ Splines/Assets/PP/DepthOfFieldEffect.cs
@@ -7,6 +7,7 @@ public class DepthOfFieldEffect : MonoBehaviour
     [HideInInspector]
     public Shader dofShader;
 
+    //Numbering passes
     const int circleOfConfusionPass = 0;
 
     [Range(0.1f, 100f)]
@@ -17,20 +18,28 @@ public class DepthOfFieldEffect : MonoBehaviour
 
     [NonSerialized] Material dofMaterial;
 
+    //Full screen pass
     private void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
+        // If the material doesn't exist, create it
         if (dofMaterial == null)
         {
             dofMaterial = new Material(dofShader);
+            // Don't need to see object in hierarchy or save it so hide flags accordingly
             dofMaterial.hideFlags = HideFlags.HideAndDontSave;
         }
 
+        // Storing the coc in a temporary buffer because we will need it in another pass
+        // Also, the texture only has one channel (RHalf) because we are only storing 1 value
         RenderTexture coc = RenderTexture.GetTemporary(source.width, source.height, 0, RenderTextureFormat.RHalf, RenderTextureReadWrite.Linear);
 
+        // Setting uniforms to use in the shader
         dofMaterial.SetFloat("_FocusDistance", focusDistance);
         dofMaterial.SetFloat("_FocusRange", focusRange);
         
+        // Blit to the coc buffer
         Graphics.Blit(source, coc, dofMaterial, circleOfConfusionPass);
+        // New blit to copy that buffer to the destination
         Graphics.Blit(coc, destination);
 
         RenderTexture.ReleaseTemporary(coc);
